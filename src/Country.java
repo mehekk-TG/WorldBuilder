e0e2150cb18776b1a2c65722bc1a116633c3f210
BT_3W2_27 <mehekk@nycstudents.net>
2024-12-17 12:59:22 -0500

    wedwqd

---
+++ src/Country.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+public class Country {
+    String name;
+    LinkedList<City> cities = new LinkedList<City>();
+    LinkedList<Terrain> terrains = new LinkedList<Terrain>();
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        String __ = "Name: " + name + "\n\nCities:\n\n";
+
+        for (int i = 0; i < cities.toArray().length; i++) {
+            __ += cities.get(i).toString() + "\n";
+        }
+
+        for (int i = 0; i < terrains.toArray().length; i++) {
+            __ += terrains.get(i).toString() + "\n";
+        }
+
+        return __;
+    }
+}
