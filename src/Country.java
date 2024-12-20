993b2fb809eac7b157a0ad145cbaf11c14df1036
BT_3W2_27 <mehekk@nycstudents.net>
2024-12-20 13:22:06 -0500

    ewnfe

--- src/Country.java
+++ src/Country.java
@@ -31,13 +31,47 @@ public class Country {
     }
 
     public void makeCity() {
+        System.out.println("Name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Ruler: ");
+        boolean ruler;
+
+
+
         System.out.print("Is this a base?: ");
         String response = scanner.nextLine();
 
         if (response.equals("yes") || response.equals("Yes")) {
-            
+            System.out.print("Is this a player base?: ");
+            response = scanner.nextLine();
+            if (response.equals("yes") || response.equals("Yes")) {
+
+                int soldiers = getInfo("Num of soldiers: ");
+                int defense = getInfo("Defense Index: ");
+                int food = getInfo("Food: ");
+                int supplies = getInfo("Supplies: ");
+                int moral = getInfo("Moral: ");
+
+                cities.add(new PlayerBase(soldiers, defense, food, supplies, moral));
+            }
         }
     }
 
 
+    public int getInfo(String prompt) {
+        System.out.print(prompt + ": ");
+        return scanner.nextInt();
+    }
+
+
+
+    public static void main(String[] args) {
+        Country country = new Country("Country");
+        System.out.println(country.toString());
+    }
+
+
+
 }
+
